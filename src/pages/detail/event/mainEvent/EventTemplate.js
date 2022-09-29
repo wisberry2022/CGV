7970a87aff9356e8f29504e8ec26d3bb3eec36f3
inseo <wisberry2022@gmail.com>
2022-09-29 21:16:12 +0900

    09/29 upload

--- src/pages/detail/event/mainEvent/EventTemplate.js
+++ src/pages/detail/event/mainEvent/EventTemplate.js
@@ -1,7 +1,4 @@
-import './CurrentEvent.scss';
-import { useSelector } from "react-redux";
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
 
 const Table = ({ eventData }) => {
   const theadList = ['이벤트기간', '지원방법', '상품', '당첨발표', '지급방법'];
@@ -27,7 +24,6 @@ const Table = ({ eventData }) => {
     </table>
   )
 }
-
 // 동적 컴포넌트(더 보기 버튼 클릭 시 EventTemplate 컴포넌트 새롭게 생성)
 const EventTemplate = ({ eventData }) => {
   return (
@@ -36,32 +32,10 @@ const EventTemplate = ({ eventData }) => {
       <div className="event_desc">
         <h5>{eventData.name}</h5>
         <Table eventData={eventData} />
-        <Link to="#">이벤트 자세히 보기</Link>
+        <Link to="#" className="btn">이벤트 자세히 보기</Link>
       </div>
     </li>
   )
 }
 
-const CurrentEvent = () => {
-  const [num, setNum] = useState(0);
-  const eventData = useSelector(state => state.currentEventReducer);
-
-  return (
-    <div className="current_event">
-      <div className="intro_box">
-        <h4>현재 진행 중인 이벤트</h4>
-        <Link to="#" className="btn">전체 이벤트 보기</Link>
-      </div>
-      <ul className="event_list">
-        {Array.from({ length: (num < 7 ? 3 + num : 9) }, (v, k) => k + 1).map((it, idx) => {
-          return (
-            <EventTemplate eventData={eventData[it - 1]} key={it} />
-          )
-        })}
-      </ul>
-      <button type="button" className="more" onClick={() => (setNum(num + 1))}>더 보기</button>
-    </div>
-  )
-}
-
-export default CurrentEvent;
+export default EventTemplate;
